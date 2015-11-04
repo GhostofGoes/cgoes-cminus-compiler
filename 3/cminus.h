8b2cd8cd20b585e42e6f2590c973ca082842deb8
Christopher Goes <goes.chris@gmail.com>
2015-11-04 14:14:31 -0800

    Removed moved code comments

--- 3/cminus.h
+++ 3/cminus.h
@@ -44,40 +44,4 @@ void typeResolution( TreeNode * node, SymbolTable * symtable );
 void treeParse( TreeNode * parent, TreeNode * node, SymbolTable * symtable );
 TreeNode * buildIOLibrary();
 
-/*
-// Creates a new node for the syntax tree
-TreeNode * makeNode( NodeKind nk, Kind k, Type t, int line, TokenData * token );
-TreeNode * makeParent( NodeKind nk, Kind k, Type t, int line, const char * svalue );
-
-// Adds a child to an existing syntax tree node
-void addChild( TreeNode * parent, TreeNode * child );
-
-// Links sib2 to the end of sib1's sibling chain. If sib1 is null, sib2 is returned.
-TreeNode * linkSiblings( TreeNode * sib1, TreeNode * sib2 );
-
-// Applies Type t to init and all its siblings
-void applyTypeToSiblings( TreeNode * init, Type t );
-
-int countSiblings( TreeNode * start );
-
-TreeNode * allocNode(); 		// Allocates and zeros a new TreeNode
-void freeTree( TreeNode * tree );	// Recursively frees and zeroes the tree
-void freeToken( TokenData * token );	// Frees and zeroes the token
-*/
-
-/* Printing and String manipulation */
-/*
-const char * iboolToString( int ib );	// Returns "true" for 1, "false" for 0
-std::string applyIndents( std::string s, int indent_count );
-const char * typeToStr( Type t );	// Returns literal string of type, eg "int" for Integer
-const char * nodekindToStr( NodeKind nk );
-std::string svalResolve( TreeNode * tree ); // Resolves the svalue of node or token as a C++ string
-std::string opToStr( TokenData * tok );
-
-void printError( int line, std::string err );
-void printWarning( int line, std::string warn );
-
-void printTreeNode( void * node );
-void printTokenData( void * token );
-*/
 #endif
