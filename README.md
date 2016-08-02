ec876aca8ae993bfb195d3637f064faeff9bd37b
Christopher Goes <ghostofgoes@gmail.com>
2016-08-01 22:20:33 -0600

    Relicensed under MIT
    GPL has come to be a thorn in my side when working on projects. I don't
    want to inflict similar pain on others.

--- README.md
+++ README.md
@@ -35,19 +35,7 @@ A compiler for Robert Heckendorn's C- (cminus) language.
     ./c- 6tests/scope35.c- -P -o scope35.tm
     tm scope35.c-
     g
-    
-    
 
 # License
-    cgoes_cminus_compiler is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    cgoes-cs445 is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with cgoes-cs445.  If not, see <http://www.gnu.org/licenses/>.
+    Code and documentation copyright 2015-2016 Christopher Goes.
+    All code and documentation released under the MIT License.
--- src/cminus.cpp
+++ src/cminus.cpp
@@ -1,22 +1,8 @@
 
 // cminus.cpp
-// Author:  Christopher Goes     
-// Contact: goes8945@vandals.uidaho.edu
-// Github: https://github.com/GhostofGoes/cgoes-cs445.git
-/* License: GPLv3
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Author:      Christopher Goes 
+// Contact:     ghostofgoes@gmail.com
+// Github:      https://github.com/GhostofGoes/cgoes-cs445.git
 
 // C++ Libraries
 #include <iostream>
